21603ec302106804a2212b85d688162751f6a716
xonatis-academy <formation@xonatis.com>
2021-03-11 15:09:36 +0100

    Adding function examples

---
+++ illustrations/fonctions/functions.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * @param $nombre c'est un entier
+ * @return resultat qui est le nombre plus 2
+ */
+function ajouter2(int $nombre): int {
+    $nombre = $nombre + 2;
+    return $nombre;
+}
+
+/**
+ * @param $nombre c'est un entier
+ * @return resultat qui est le nombre plus 3
+ */
+function ajouter3(int $nombre): int {
+    $nombre = $nombre + 3;
+    return $nombre;
+} 
+
+?>
---
+++ illustrations/fonctions/test.php
@@ -0,0 +1,13 @@
+<?php
+
+include __DIR__.'/functions.php';
+
+$texte = 3;
+
+$caca = ajouter2($texte);
+$toto = ajouter3($caca);
+
+var_dump($toto);
+
+?>
+
