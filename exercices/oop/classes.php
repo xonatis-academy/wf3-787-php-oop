2a02a9c611d4096549ce08e9f37148651320ef14
xonatis-academy <formation@xonatis.com>
2021-03-12 12:01:09 +0100

    Adding Animal class

---
+++ exercices/oop/classes.php
@@ -0,0 +1,22 @@
+<?php
+
+class Animal {
+
+    public $age;
+    public $groupeSanguin;
+    public $taille;
+    public $poids;
+    private $sang;
+
+    public function mange() {
+
+    }
+
+    protected function dort() {
+        
+    }
+
+}
+
+?>
+
