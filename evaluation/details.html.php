58609bfba12fb1bc9ac1690c77fea86026f640c4
xonatis-academy <formation@xonatis.com>
2021-03-15 11:29:50 +0100

    Adding header and footer

--- evaluation/details.html.php
+++ evaluation/details.html.php
@@ -1,41 +1,28 @@
-<!DOCTYPE html>
-<html>
-
-<head>
-    <meta charset="UTF-8">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
-</head>
-
-<body>
-
-    <div class="container">
-        <h1 class="display-4 text-center"><?php echo $produit->titre; ?></h1>
-        <div class="row">
-            <div class="col">
-                <p>
-                    <strong>Nom du produit :</strong>
-                    <?php echo $produit->titre; ?>
-                </p>
-                <p>
-                    <strong>Description du produit :</strong>
-                    <?php echo $produit->description; ?>
-                </p>
-                <p>
-                    <strong>Prix du produit :</strong>
-                    <?php echo $produit->prix; ?> euros
-                </p>
-                <a href="index.php" class="btn btn-secondary">Retour a l'affichage</a>
-            </div>
-            <div class="col">
-                <img class="w-100" src="<?php echo $produit->photo ?>" />
-            </div>
-        </div>
+<?php
+include __DIR__ . '/views/header.html.php';
+?>
+<h1 class="display-4 text-center"><?php echo $produit->titre; ?></h1>
+<div class="row">
+    <div class="col">
+        <p>
+            <strong>Nom du produit :</strong>
+            <?php echo $produit->titre; ?>
+        </p>
+        <p>
+            <strong>Description du produit :</strong>
+            <?php echo $produit->description; ?>
+        </p>
+        <p>
+            <strong>Prix du produit :</strong>
+            <?php echo $produit->prix; ?> euros
+        </p>
+        <a href="index.php" class="btn btn-secondary">Retour a l'affichage</a>
     </div>
+    <div class="col">
+        <img class="w-100" src="<?php echo $produit->photo ?>" />
+    </div>
+</div>
 
-    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous">
-    </script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-Piv4xVNRyMGpqkS2by6br4gNJ7DXjqk09RmUpJ8jgGtD7zP9yug3goQfGII0yAns" crossorigin="anonymous">
-    </script>
-</body>
-
-</html>
+<?php
+include __DIR__ . '/views/footer.html.php';
+?>
--- evaluation/index.html.php
+++ evaluation/index.html.php
@@ -1,63 +1,50 @@
-<!DOCTYPE html>
-<html>
+<?php
+include __DIR__ . '/views/header.html.php';
+?>
 
-<head>
-    <meta charset="UTF-8">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
-</head>
+<h1 class="display-4 text-center">Liste des produits</h1>
+<a href="add.php" class="btn btn-primary">Ajouter un produit</a>
+<table class="table table-striped w-100 mx-auto">
+    <tr>
+        <th>Nom</th>
+        <th>Prix</th>
+        <th>Image</th>
+        <th>Type</th>
+        <th>Description</th>
+        <th>Action</th>
+    </tr>
 
-<body>
+    <?php
+    for ($i = 0; $i < count($tableau); ++$i) {
+    ?>
 
-    <div class="container">
-        <h1 class="display-4 text-center">Liste des produits</h1>
-        <a href="add.php" class="btn btn-primary">Ajouter un produit</a>
-        <table class="table table-striped w-100 mx-auto">
-            <tr>
-                <th>Nom</th>
-                <th>Prix</th>
-                <th>Image</th>
-                <th>Type</th>
-                <th>Description</th>
-                <th>Action</th>
-            </tr>
+        <tr>
+            <td class="w-25"><?php echo $tableau[$i]->titre ?></td>
+            <td><?php echo $tableau[$i]->prix ?> euros</td>
+            <td>
+                <img class="w-100" src="<?php echo $tableau[$i]->photo ?>" />
+            </td>
+            <td><?php echo $tableau[$i]->type ?></td>
+            <td><?php
+                $taille = strlen($tableau[$i]->description);
+                if ($taille > 25) {
+                    echo substr($tableau[$i]->description, 0, 25) . '...';
+                } else {
+                    echo $tableau[$i]->description;
+                }
 
-            <?php
-            for ($i = 0; $i < count($tableau); ++$i) {
-            ?>
+                ?></td>
+            <td>
+                <a href="details.php?id=<?php echo $tableau[$i]->id ?>" class="btn btn-secondary">Details</a>
+            </td>
+        </tr>
 
-                <tr>
-                    <td class="w-25"><?php echo $tableau[$i]->titre ?></td>
-                    <td><?php echo $tableau[$i]->prix ?> euros</td>
-                    <td>
-                        <img class="w-100" src="<?php echo $tableau[$i]->photo ?>" />
-                    </td>
-                    <td><?php echo $tableau[$i]->type ?></td>
-                    <td><?php
-                        $taille = strlen($tableau[$i]->description);
-                        if ($taille > 25) {
-                            echo substr($tableau[$i]->description, 0, 25) . '...';
-                        } else {
-                            echo $tableau[$i]->description;
-                        }
+    <?php
+    }
+    ?>
 
-                        ?></td>
-                    <td>
-                        <a href="details.php?id=<?php echo $tableau[$i]->id ?>" class="btn btn-secondary">Details</a>
-                    </td>
-                </tr>
+</table>
 
-            <?php
-            }
-            ?>
-
-        </table>
-
-    </div>
-
-    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous">
-    </script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-Piv4xVNRyMGpqkS2by6br4gNJ7DXjqk09RmUpJ8jgGtD7zP9yug3goQfGII0yAns" crossorigin="anonymous">
-    </script>
-</body>
-
-</html>
+<?php
+include __DIR__ . '/views/footer.html.php';
+?>
